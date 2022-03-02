7b9e3cfa92997efb09d417c69595f06203bb4773
kasai <kasaipreston@gmail.com>
2022-03-02 14:47:31 -0800

    gettting models sorted

--- models/index.js
+++ models/index.js
@@ -1,13 +1,30 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-// User.hasMany(Post, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
-// Project.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
-module.exports = { User };
+module.exports = {User, Post, Comment};
