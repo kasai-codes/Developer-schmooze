d56e890e82cb32c2eb9a7e559d36a65eb0b36cd7
kasai <kasaipreston@gmail.com>
2022-03-05 19:34:27 -0800

    more debugging

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,108 +1,113 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
+// const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require("../utils/auth");
 
-router.get('/', (req, res) => {
-    console.log(req.session);
-    
-    Post.findAll({
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'content'
-      ],
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
       include: [
         {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
           model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', {
-            posts,
-            loggedIn: req.session.loggedIn
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
-
-  router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('signup');
-  });
-
-  router.get('/post/:id', (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'content'
+          attributes: ["username"],
+        },
       ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-  
-        // serialize the data
-        const post = dbPostData.get({ plain: true });
-  
-        // pass data to template
-        res.render('single-post', {
-            post,
-            loggedIn: req.session.loggedIn
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+      order: [["created_at", "DESC"]],
+    });
+
+    const posts = postData.map((post) => post.toJSON());
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+      username: req.session.username,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username", "id"],
+        },
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ["username", "id"],
+          },
+        },
+      ],
+      order: [[Comment, "createdAt", "DESC"]],
+    });
+
+    const post = postData.toJSON();
+
+    res.render("post", {
+      post,
+      logged_in: req.session.logged_in,
+      username: req.session.username,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  const postsData = await Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    order: [["createdAt", "DESC"]],
+  });
+  const posts = postsData.map((post) => post.toJSON());
+  res.render("dashboard", {
+    posts,
+    user_id: req.session.user_id,
+    username: req.session.username,
+    logged_in: req.session.logged_in,
+  });
+});
+
+
+router.get("/create", withAuth, (req, res) => {
+  res.render("create", {
+    user_id: req.session.user_id,
+    username: req.session.username,
+    logged_in: req.session.logged_in,
+  });
+});
+
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+  const postData = await Post.findByPk(req.params.id);
+  const post = postData.toJSON();
+  post.content = post.content.replaceAll("<br />", "\n");
+
+
+  res.render("edit", {
+    post,
+    user_id: req.session.user_id,
+    username: req.session.username,
+    logged_in: req.session.logged_in,
+  });
+});
+
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -2,56 +2,49 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-// create our User model
+
 class User extends Model {
-  // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+      console.log(loginPw, this.password);
+      return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// define table columns and configuration
-User.init(
-  {
-    id: {
+User.init({
+  id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-    },
-    username: {
+  },
+  username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-    },
-    password: {
+  },
+  password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+          len: [8],
       },
-    },
   },
-  {
-    hooks: {
+}, {
+  hooks: {
       beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
       },
-    },
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "user",
-  }
-);
+  },
+  sequelize,
+  timestamps: true,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'user',
+});
 
 module.exports = User;
